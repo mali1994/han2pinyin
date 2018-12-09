91223d15ff0b33ab525301ba5d2ab1a46bdea71e
unknown <m_li@foxmail.com>
2018-12-09 23:42:31 +0800

    init

---
+++ hantopinyin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   hantopinyin.py
+@Time    :   2018/12/09 23:40:38
+@Author  :   龙国星海 
+@Version :   1.0
+@Contact :   m_li@foxmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
+import pypinyin
